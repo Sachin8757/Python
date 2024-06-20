62e6a614c9fb5203631cf03f2df15692a27bf903
priyansu kumar <priyanshu727989@gmail.com>
2024-06-20 12:48:18 +0530

    new commit

---
+++ class_02/class_02.py
@@ -0,0 +1,40 @@
+#variable
+# a=5 # a is a variable wich is store inter value
+# b=3
+# print(a+b)
+
+# Data Type
+#  integer
+#  floating point number
+#  strings
+#  boleans
+#  None
+
+
+# Oprators
+# a=7
+# a +=5
+# print(a)
+
+# a=4
+# b=8
+# print(a>b)
+# print(a<b)
+
+#Type ()function 
+# a=5
+# b=type(a)
+# print(b)
+
+# c="sachin"
+# d=type(c)
+# print(d)
+
+#Type casting
+# a=5.5
+# b=type(a)
+# print(b)
+# c=int(a)
+# d=type(c)
+# print(d)
+
