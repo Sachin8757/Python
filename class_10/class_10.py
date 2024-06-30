3e7629696127e03f3b4dd5f58cac5f15ce84bfde
Sachin kumar <130838646+sachinkuamr@users.noreply.github.com>
2024-06-30 09:21:13 +0530

    add class_10 folder

---
+++ class_10/class_10.py
@@ -0,0 +1,37 @@
+# class
+
+# class emp:
+#     def getdata(self):
+#         print(f"name={self.name},salary={self.salary},language={self.language}")
+    
+#     def putdata(self):
+#         self.name=input('enter name of employee')
+#         self.salary=int(input("enter salary of emp"))
+#         self.language=input("enter language of employee")
+        
+# sachin=emp()
+# sachin.putdata()
+# sachin.getdata()
+
+# constructor
+
+# class emp:
+#     def __init__(self) -> None:
+#        self.name=""
+#        self.salary=000
+#        self.language=""
+#        print("constructor is calling")
+    
+#     def getdata(self):
+#         print(f"name={self.name},salary={self.salary},language={self.language}")
+    
+#     def putdata(self):
+#         self.name=input('enter name of employee:-')
+#         self.salary=int(input("enter salary of emp:-"))
+#         self.language=input("enter language of employee:-")
+        
+# sachin=emp()
+# sachin.putdata()
+# sachin.getdata()
+
+
